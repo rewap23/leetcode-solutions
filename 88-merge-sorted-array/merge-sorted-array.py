26607f10cbe4843dc49117a37616c6449c47c32d
Rewa Bathula <rewapraga@gmail.com>
2026-08-05 19:12:21 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (0.76%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -8,10 +8,27 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
         # taking four parameters - two arrays, two integers that are the number of elements in the arrays we are adding
+        # solution should be stored in nums1 not returned
+        # brute force - replacing index m onwards elements of nums1 with nums2
+        #nums1[m:] = nums2
+        #nums1.sort()
+        # this isnt the best time complexity - calling .sort() function which is 
+        # O(n log n)
+
+        # best solution
+        while (n-1) >= 0:
+            if (m-1) >= 0 and (nums1[m-1] > nums2[n-1]):
+                nums1[(m+n)-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[(m+n)-1] = nums2[n-1]
+                n -= 1
+            
+
+
+
         
-        # brute force - replace the 
-        nums1[m:] = nums2
-        nums1.sort()
+
         
 
 
