31202dffa38d021371212d513f7d3139ba164496
Rewa Bathula <rewapraga@gmail.com>
2026-08-10 16:16:43 -0700

    Time: 8 ms (14.15%) | Memory: 13.8 MB (43.30%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -10,7 +10,8 @@ class Solution(object):
         unique = 1
         for right in range(1, len(nums)):
             if nums[right] != nums[right-1]:
-                nums[unique] = nums[right]
+                nums[unique] = nums[right] 
+                #  This is the "in-place modification" step. It essentially shifts unique values to the front of the list, overwriting the unnecessary duplicate values that were previously sitting
                 unique += 1
         return unique
 
