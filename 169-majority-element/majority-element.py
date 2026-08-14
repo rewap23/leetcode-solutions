c751b3ec4593d9fe6edff6878965db7e58ec7b74
Rewa Bathula <rewapraga@gmail.com>
2026-08-13 22:30:49 -0700

    Time: 0 ms (100.00%) | Memory: 13.6 MB (73.62%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # creating dict with key value pairs
+        numsMap = defaultdict(int)
+
+        for num in nums:
+            numsMap[num] += 1
+
+        # key is the number
+        # value is the amount of times it appears
+        for key, value in numsMap.items():
+            if value > (len(nums) // 2):
+                return key
+
+        
