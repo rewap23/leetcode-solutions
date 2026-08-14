848571f4c4e546cf98845e2cca7b43c28c41e7e6
Rewa Bathula <rewapraga@gmail.com>
2026-08-13 23:00:01 -0700

    Time: 5 ms (44.14%) | Memory: 14 MB (7.15%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -4,6 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        theSum = ((len(nums)) * ((len(nums)) + 1)) // 2
-        actualSum = sum(nums)
-        return theSum - actualSum
+        # theSum = ((len(nums)) * ((len(nums)) + 1)) // 2
+        #actualSum = sum(nums)
+        #return theSum - actualSum
+
+
+        # hash set - 0(n) space and time
+        numsSet = set(nums) # set takes 0(n)
+
+        for num in range(len(nums)+1):
+            if num not in numsSet:
+                return num
+
