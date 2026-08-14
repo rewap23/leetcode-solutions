53e70c9211c02ed3ad76e49e401b0bc13425045f
Rewa Bathula <rewapraga@gmail.com>
2026-08-13 23:44:03 -0700

    Time: 15 ms (20.10%) | Memory: 15.2 MB (5.81%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # O(1) space
+        
+        
+        numsMap = {} # creating a hash map 
+
+        for num in nums:
+            numsMap[num] = numsMap.get(num, 0) + 1
+        
+        for key, value in numsMap.items(): # looking at how many times a value appears
+            if value == 1:
+                return key
