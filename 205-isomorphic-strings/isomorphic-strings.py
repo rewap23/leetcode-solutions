8506c1bc93ab1b42b00e338ed1ba94ed1f1814db
Rewa Bathula <rewapraga@gmail.com>
2026-08-16 11:38:12 -0700

    Time: 19 ms (9.49%) | Memory: 12.9 MB (94.47%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -5,14 +5,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # O(n^2) time
+        # 0(n) space
         sMap = []
         tMap = []
 
         for idx in s:
-            sMap.append(s.index(idx))
+            sMap.append(s.index(idx)) # index function gets the index of that character
+            # so for "egg" it looks at e and saves 0, and then looks at g and saves 1 and then 1 again bc it appears first at 1
         for idx in t:
-            tMap.append(t.index(idx))
-
+            tMap.append(t.index(idx)) 
+            # does the same logic as above
         if sMap == tMap:
             return True
 
