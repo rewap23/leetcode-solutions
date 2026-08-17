114ccad7d06a0f05e2bd3590406854fd6b626199
Rewa Bathula <rewapraga@gmail.com>
2026-08-17 14:19:03 -0700

    Time: 7 ms (38.49%) | Memory: 15.1 MB (74.29%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        five = 0
+        ten = 0
+        for n in bills:
+            if n == 5:
+                five += 1
+            if n == 10:
+                if five == 0:
+                    return False
+                ten += 1
+                five -= 1
+            if n == 20:
+                if five > 0 and ten > 0:
+                    five -= 1
+                    ten -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        
+        return True
