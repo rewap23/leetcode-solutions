1660fd12cb1f4432b0719dd1a663a299d7022e21
Rewa Bathula <rewapraga@gmail.com>
2026-08-18 14:56:44 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (74.03%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        stack = []
+        for log in logs:
+            if log == '../':
+                if stack:
+                    stack.pop()
+                else:
+                    continue
+            elif log == './':
+                continue
+            else:
+                stack.append(log)
+
+        return len(stack)
