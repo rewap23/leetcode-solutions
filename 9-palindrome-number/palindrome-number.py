6f6c8b0620ef31da1212f891282ace6442912863
Rewa Bathula <rewapraga@gmail.com>
2026-08-19 22:22:51 -0700

    Time: 10 ms (59.86%) | Memory: 12.3 MB (55.81%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,20 +4,25 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        # 
-        xlist = list(str(x)) 
-        # base case scenario
         if x < 0:
             return False
-
-        right = len(xlist) - 1
-        
-        for left in range(len(xlist)):
-            xlist[left] = xlist[right]
-            xlist[right] = xlist[left]
-            right -= 1
-
-        if "".join(xlist) == str(x):
+        s = str(x)
+        if s == s[::-1]:
             return True
-        else: 
+        else:
             return False
+        
+        # two pointers solution
+         #xlist = list(str(x)) 
+        # base case scenario
+        #if x < 0:
+            #return False
+        #right = len(xlist) - 1
+        #for left in range(len(xlist)):
+            #xlist[left] = xlist[right]
+            #xlist[right] = xlist[left]
+            #right -= 1
+        #if "".join(xlist) == str(x):
+            #return True
+        #else: 
+            #return False
