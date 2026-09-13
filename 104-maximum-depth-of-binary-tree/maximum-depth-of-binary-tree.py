8745d9718d8ee4b09d3aeba58e7b3dbe0e01ae33
Rewa Bathula <rewapraga@gmail.com>
2026-09-12 23:14:19 -0700

    Time: 12 ms (15.13%) | Memory: 24.6 MB (73.55%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -10,6 +10,7 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        # O(n) time and space
         # always do base case first
         if root is None:
             return 0
