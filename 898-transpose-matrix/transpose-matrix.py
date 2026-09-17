c5a199289ec4e118b8670b49d9381af3d13a4a91
Rewa Bathula <rewapraga@gmail.com>
2026-09-17 16:34:37 -0700

    Time: 3 ms (27.36%) | Memory: 12.9 MB (77.29%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def transpose(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # OA prep
+        # one line - transpose a matrix 
+        # transpose = list(zip(*matrix))
+        #initalize rows and columns of the matrix
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        # key thing to remember --> initalizing an resulting transposed matrix grid
+        result = [[0] * rows for _ in range(cols)] 
+
+        for i in range(cols):
+            for j in range(rows):
+                result[i][j] = matrix[j][i]
+
+        return result
+
