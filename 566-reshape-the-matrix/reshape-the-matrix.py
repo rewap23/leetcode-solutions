98cefdd0353bd468bf64ed1ef21e8c1006d2911a
Rewa Bathula <rewapraga@gmail.com>
2026-09-19 12:21:24 -0700

    Time: 1 ms (53.92%) | Memory: 13.4 MB (5.60%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def matrixReshape(self, mat, r, c):
+        """
+        :type mat: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        rows = len(mat)
+        cols = len(mat[0])
+
+        # create a grid of the dimensions of the new matrix
+        result = [[0] * c for _ in range(r)]
+
+
+        if (rows * cols) != (r * c):
+            return mat
+        
+        for i in range(rows * cols):
+            result[i // c][i % c] = mat[i // cols][i % cols]
+                
+        return result
+
+
